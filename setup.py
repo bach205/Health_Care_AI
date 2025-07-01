bbf72d416ad2122cff83e0aa977fce1a6b5b86ec
Huy Bach <dhbach13@gmail.com>
2025-07-02 06:29:37 +0700

    Fix: add create chromadb's storage in readme

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def __main__():
     )
     chromadb_persistent_location = Path(__file__).parent / "vector_store" / "chromadb"
     client = chromadb.PersistentClient(path=str(chromadb_persistent_location))
-    collection = client.create_collection(name="health_care_ai_documents",embedding_function=embedding_fn)
+    client.create_collection(name="health_care_ai_documents",embedding_function=embedding_fn)
     # client.delete_collection("health_care_ai_documents")
 if __name__ == "__main__":
     __main__()
